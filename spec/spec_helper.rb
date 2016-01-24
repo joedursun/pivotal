270610ffb4dfacbaa898f94b6dcf33eb4b265f0f
Joe Dursun <joedursun@gmail.com>
2016-01-24 14:51:22 -0500

    Stub constants to ensure tests run

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -4,3 +4,8 @@ require 'pivotal/request'
 require 'pivotal/label'
 require 'pivotal/story'
 require 'pivotal/project'
+
+RSpec.configure do |c|
+  c.before(:example) { stub_const("Pivotal::Request::TOKEN", 'abcdefgh123456789zyxwvut') }
+  c.before(:example) { stub_const("Pivotal::Request::PROJECT", '1234567') }
+end
