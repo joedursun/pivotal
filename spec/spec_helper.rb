300d3ce925d1fc84d70b7e081e96cb75cb90b5d1
Joe Dursun <joedursun@gmail.com>
2016-04-07 11:20:29 -0400

    Start SimpleCov before loading app

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,12 +1,13 @@
-require 'rspec'
-require 'pivotal'
 require 'simplecov'
-
 SimpleCov.start
+
 if ENV['CI']=='true'
   require 'codecov'
   SimpleCov.formatter = SimpleCov::Formatter::Codecov
 end
+
+require 'rspec'
+require 'pivotal'
 
 RSpec.configure do |c|
   c.before(:example) { stub_const("Pivotal::Request::TOKEN", 'abcdefgh123456789zyxwvut') }
